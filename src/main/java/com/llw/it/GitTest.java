a6f95f09a3430403913de823448f1102a865fdc7
liulw <liulw@qq.com>
2022-07-02 11:10:59 +0800

    master push

--- src/main/java/com/llw/it/GitTest.java
+++ src/main/java/com/llw/it/GitTest.java
@@ -8,5 +8,6 @@ public class GitTest {
         System.out.println("4444444444");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("push");
     }
 }
